8d2189657b8790308192be1e93c251afbacbdd42
rtoaz <2685700760@qq.com>
2025-09-30 22:41:14 +0800

    Update 全局物体漂浮最终优化版.lua

--- 全局物体漂浮最终优化版.lua
+++ 全局物体漂浮最终优化版.lua
@@ -294,9 +294,9 @@ local function CreateMobileGUI()
 
     -- 十字架方向按钮
     local dirButtons = {
-        {name="上", dir="up", pos=UDim2.new(0.35,0,0,190)},
-        {name="下", dir="down", pos=UDim2.new(0.35,0,0,260)},
-        {name="左", dir="left", pos=UDim2.new(0.2,0,0,225)},
+        {name="上", dir="up", pos=UDim2.new(0.3,0,0,190)},
+        {name="下", dir="down", pos=UDim2.new(0.3,0,0,260)},
+        {name="左", dir="left", pos=UDim2.new(0.1,0,0,225)},
         {name="右", dir="right", pos=UDim2.new(0.5,0,0,225)},
         {name="前", dir="forward", pos=UDim2.new(0.05,0,0,225)}, -- 左的左边
         {name="后", dir="back", pos=UDim2.new(0.65,0,0,225)},    -- 右的右边
